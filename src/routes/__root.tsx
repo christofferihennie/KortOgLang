d9e6d380acea7eb00704aaa52aff50394b34928a
Christoffer H <developer@hennie.dev>
2026-08-07 23:09:11 +0200

    feat: restore convex dir and inital tanstack setup

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,10 +1,14 @@
+import { QueryClient } from "@tanstack/react-query";
+import { createRootRouteWithContext } from "@tanstack/react-router";
 import { HeadContent, Scripts, createRootRoute } from "@tanstack/react-router"
 import { TanStackRouterDevtoolsPanel } from "@tanstack/react-router-devtools"
 import { TanStackDevtools } from "@tanstack/react-devtools"
 
 import appCss from "../styles.css?url"
 
-export const Route = createRootRoute({
+export const Route = createRootRouteWithContext<{
+  queryClient: QueryClient;
+}>()({
   head: () => ({
     meta: [
       {
